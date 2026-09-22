994535dbf8021043abd01f2a9bda3ea43facc9c1
hasan jim <hasanjim2345@gmail.com>
2026-09-23 00:23:01 +0600

    fixed particle number

--- src/components/ParticleBackground.jsx
+++ src/components/ParticleBackground.jsx
@@ -15,7 +15,7 @@ function ParticleBackground() {
         fullScreen: { enable: true, zIndex: -1 },
         background: { color: "transparent" },
         particles: {
-          number: { value: 450, density: { enable: true, area: 900 } },
+          number: { value: 600, density: { enable: true, area: 900 } },
           color: { value: "#7c1fd6" },
           shape: { type: "circle" },
           opacity: {
